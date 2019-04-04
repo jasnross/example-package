298e6f44bfee23be712669b525f66d4aaa62511b
Jason Ross <JasonR@BitTitan.com>
2019-04-04 13:22:42 -0700

    Initial commit

---
+++ Install.ps1
@@ -0,0 +1,7 @@
+# Use this script to install PowerShell modules required by this package.
+#
+# See: https://docs.microsoft.com/en-us/powershell/module/powershellget/install-module?view=powershell-6
+# 
+# Example:
+#
+# Install-Module -Name AzureRM -RequiredVersion 2.3.0 
